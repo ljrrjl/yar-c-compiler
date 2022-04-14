9a219e84a2c5ad46f0ad2cd348b749c0189c469b
ljr_rjl <420552694@qq.com>
2022-04-14 12:02:56 +0000

    sync 2022-04-14

--- include/Generator.h
+++ include/Generator.h
@@ -6,7 +6,7 @@ class Generator
 {
 public:
 	Generator(const FileID& file_id): _file_id(file_id){}
-	virtual std::shared_ptr<Sentence> generate(){ return std::make_shared<Sentence>(); }
+	virtual std::shared_ptr<Sentence> generate() = 0;
 protected:
 	FileID _file_id;
 };
